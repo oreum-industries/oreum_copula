676c5c236fe4ac1d80173cee04fd49421e84e58a
Jonathan Sedar <jonsedar@users.noreply.github.com>
2024-12-04 15:57:55 +0400

    Separate out and refresh the 3 demo notebooks and all associated code (#1)
    * + WIP overhaul NB001 to latest workflow practices
    + added utils
    
    * + WIP NB001
    
    * + WIP workflow on NB100
    
    * + WIP NB100 workflow
    
    * + WIP nb100
    
    * + WIP NB100
    
    * + completed NB100
    
    * + WIP NB101 / modela1
    + tonnes of work finding bugs in pymc
    
    * + WIP NB101
    
    * + completed the refactor on NB101, ran E@E
    
    * + completed model A2 and NB102 E2E

---
+++ src/engine/utils.py
@@ -0,0 +1,84 @@
+# src.engine.utils.py
+# copyright 2024 Oreum OÜ
+"""Assorted display and plotting utilities for the project"""
+
+import pandas as pd
+import seaborn as sns
+from IPython.display import Markdown, display
+from oreum_core import curate
+from oreum_core import model_pymc as mt
+
+__all__ = ["ProjectDFXCreator", "display_rvs"]
+
+sns.set_theme(
+    style="darkgrid",
+    palette="muted",
+    context="notebook",
+    rc={"figure.dpi": 72, "savefig.dpi": 144, "figure.figsize": (12, 4)},
+)
+
+
+class ProjectDFXCreator:
+    """Convenience class to process the main dataset that's used
+    throughout this project into `dfx` for model input. Saves us having to
+    redeclare it in each notebook.
+    """
+
+    def __init__(self, kind="m0"):
+        # NOTE: The death event is an endog feature, but is recorded in our data
+        # as a bool so for convenience we will patsy transform the death event
+        # as if it were part of the linear model, then remove it from fts_ex and
+        # put into fts_en, and rearrange dfx accordingly also
+        self.fml_tfmr = "1"
+        self.dfcmb = None
+        self.dfscale = None
+        self.factor_map = None
+        if kind == "m0":
+            self.ft_en = ["m0"]
+        else:  ## m1
+            self.ft_en = ["m1"]
+
+    def get_dfx(self, df: pd.DataFrame, in_sample: bool = True) -> pd.DataFrame:
+        """Reshape, transform & standardize df to create dfx for model input"""
+
+        # 1. Create dfcmb (reshaped) from df
+        if in_sample:
+            reshaper = curate.DatasetReshaper()
+            self.dfcmb = reshaper.create_dfcmb(df)
+        else:
+            assert self.dfcmb is not None, "run in_sample = True first"
+
+        # 2. create Transformer based on dfcmb
+        tfmr = curate.Transformer()
+        _ = tfmr.fit_transform(self.fml_tfmr, self.dfcmb, propagate_nans=True)
+        self.factor_map = tfmr.factor_map
+
+        # 3. Transform df according to dfcmb
+        df_ex = tfmr.transform(df, propagate_nans=True)
+
+        # 4. Standardize
+        stdr = curate.Standardizer(tfmr)
+        if in_sample:
+            df_exs = stdr.fit_standardize(df_ex)
+            self.dfscale, _ = stdr.get_scale()  # unused in this process, because no dfb
+        else:
+            stdr.set_scale(self.dfscale)
+            df_exs = stdr.standardize(df_ex)
+
+        df_en = df[self.ft_en].copy()
+        kws_mrg = dict(how="inner", left_index=True, right_index=True)
+        dfx = pd.merge(df_en, df_exs, **kws_mrg).rename(
+            columns=lambda x: tfmr.snl.clean_patsy(x)
+        )
+
+        fts_ex = [v for v in dfx.columns.values if v not in self.ft_en]
+        dfx = dfx.reindex(columns=self.ft_en + fts_ex)
+
+        return dfx
+
+
+def display_rvs(mdl: mt.BasePYMCModel):
+    """Convenience to display RVS and values"""
+    _ = display(f"RVs for {mdl.mdl_id}")
+    if mdl.model is not None:
+        _ = [display(Markdown(s)) for s in mt.print_rvs(mdl)]
---
+++ src/model/copula.py
@@ -0,0 +1,378 @@
+# src.model.copula.py
+# copyright 2024 Oreum OÜ
+"""Basic models in family copula"""
+
+import numpy as np
+import pandas as pd
+import pymc as pm
+import pytensor.tensor as pt
+from oreum_core import model_pymc as mt
+
+__all__ = ["ModelA0", "ModelA1", "ModelA2"]
+
+
+class ModelA0(mt.BasePYMCModel):
+    """Basic naive architecture: priors, marginal likelihoods, but no copula
+    Core components used from oreum_core.model.BasePYMCModel
+    As used by 100_ModelA0.ipynb
+    """
+
+    name = "mdla0"
+    version = "1.3.0"
+
+    def __init__(
+        self,
+        obs_m0: pd.DataFrame,
+        obs_m1: pd.DataFrame,
+        dfx_creatord: dict,
+        *args,
+        **kwargs,
+    ):
+        """Expects 2 dataframes for obs, each arranged as:
+            y ~ x, aka pd.concat((dfx_en, dfx_exs), axis=1)
+        Also expects `dfx_creatord` dict as {m0: dfx_creator, ...}
+        """
+        super().__init__(*args, **kwargs)
+        self.obs_nm = kwargs.pop("obs_nm", "obs")
+        self.rng = np.random.default_rng(seed=self.rsd)
+        self.sample_kws.update(dict(target_accept=0.80))
+
+        # set obs, coords, and do data validity checks
+        self.obs_m0 = obs_m0.copy()
+        self.obs_m1 = obs_m1.copy()
+        assert len(self.obs_m0) == len(self.obs_m1)
+        self.ft_en_m0 = dfx_creatord["m0"].ft_en
+        self.ft_en_m1 = dfx_creatord["m1"].ft_en
+        self.factor_map_m0 = dfx_creatord["m0"].factor_map
+        self.factor_map_m1 = dfx_creatord["m0"].factor_map
+        # setup coords with dict expansion and additional structural names
+        self.coords = dict(
+            x0_nm=self.obs_m0.columns.drop(self.ft_en_m0).values,
+            x1_nm=self.obs_m1.columns.drop(self.ft_en_m1).values,
+            # x0_nm={k: list(d.keys()) for k, d in factor_map_m0.items()},
+            # x1_nm={k: list(d.keys()) for k, d in factor_map_m1.items()},
+            s_nm=["s0", "s1"],
+            m_nm=["m0", "m1"],
+            mhat_nm=["m0hat", "m1hat"],
+        )
+
+    def _build(self):
+        """Builds and returns the model. Also sets self.model"""
+        self.coords.update(dict(oid=self.obs_m0.index.values))  # (i, )
+        self.n = len(self.obs_m0)
+        m_r = pd.concat(
+            [self.obs_m0[self.ft_en_m0], self.obs_m1[self.ft_en_m1]], axis=1
+        )
+        y_r = m_r.product(axis=1)
+        x0_r = self.obs_m0.drop(self.ft_en_m0, axis=1)
+        x1_r = self.obs_m1.drop(self.ft_en_m1, axis=1)
+
+        with pm.Model(coords=self.coords) as self.model:
+            # 0. Create (Mutable)Data containers for obs (Y, X)
+            m = pm.Data("m", m_r, dims=("oid", "m_nm"))
+            _ = pm.Data("y", y_r, dims="oid")  # convenience for post-processing
+            x0 = pm.Data("x0", x0_r, dims=("oid", "x0_nm"))
+            x1 = pm.Data("x1", x1_r, dims=("oid", "x1_nm"))
+
+            # 1. Define latent params for observed Marginals, regressing on F(X)
+            b_m0 = pm.Normal("beta_m0", mu=0.0, sigma=1.0, dims="x0_nm")
+            b_m1 = pm.Normal("beta_m1", mu=0.0, sigma=1.0, dims="x1_nm")
+            mu = pt.stack([pt.dot(x0, b_m0.T), pt.dot(x1, b_m1.T)], axis=1)
+            s = pm.InverseGamma("sigma", alpha=5.0, beta=4.0, dims="s_nm")
+
+            # 2. Define marginals and Evidence against observed
+            mhat = pm.LogNormal(
+                "mhat", mu=mu, sigma=s, observed=m, dims=("oid", "mhat_nm")
+            )
+
+            # 3. Post-process for forward / PPC estimation: create joint marginal
+            # product yhat.
+            # Sidenote pymc also puts yhat into posterior but we must ignore it,
+            # since it has the direct values of observed m, and is not made from
+            # mhat. We can get yhat from sample_posterior_predictive
+            _ = pm.Deterministic("yhat", pm.math.prod(mhat, axis=1), dims="oid")
+
+        self.rvs_marg = ["sigma", "beta_m0", "beta_m1"]
+        self.rvs_ppc = ["mhat"]
+        self.rvs_det = ["yhat"]
+
+        return self.model
+
+
+class ModelA1(mt.BasePYMCModel):
+    """Partial architecture (extends ModelA0)
+    New:
+      + Gaussian copula (but without Jacobian adjustment)
+      + Potentials to overcome ObservedRVs in likelihood
+    Core components used from oreum_core.model.BasePYMCModel
+    As used by 101_ModelA1.ipynb
+    """
+
+    name = "mdla1"
+    version = "1.1.0"
+
+    def __init__(
+        self,
+        obs_m0: pd.DataFrame,
+        obs_m1: pd.DataFrame,
+        dfx_creatord: dict,
+        *args,
+        **kwargs,
+    ):
+        """Expects 2 dataframes for obs, each arranged as:
+            y ~ x, aka pd.concat((dfx_en, dfx_exs), axis=1)
+        Also expects `dfx_creatord` dict as {m0: dfx_creator, ...}
+        """
+        super().__init__(*args, **kwargs)
+        self.obs_nm = kwargs.pop("obs_nm", "obs")
+        self.rng = np.random.default_rng(seed=self.rsd)
+        self.sample_kws.update(dict(target_accept=0.80))
+
+        # set obs, coords, and do data validity checks
+        self.obs_m0 = obs_m0.copy()
+        self.obs_m1 = obs_m1.copy()
+        assert len(self.obs_m0) == len(self.obs_m1)
+        self.ft_en_m0 = dfx_creatord["m0"].ft_en
+        self.ft_en_m1 = dfx_creatord["m1"].ft_en
+        self.factor_map_m0 = dfx_creatord["m0"].factor_map
+        self.factor_map_m1 = dfx_creatord["m0"].factor_map
+        # setup coords with dict expansion and additional structural names
+        self.coords = dict(
+            x0_nm=self.obs_m0.columns.drop(self.ft_en_m0).values,
+            x1_nm=self.obs_m1.columns.drop(self.ft_en_m1).values,
+            s_nm=["s0", "s1"],
+            m_nm=["m0", "m1"],
+            mhat_nm=["m0hat", "m1hat"],
+            u_nm=["u0", "u1"],
+            c_nm=["c0", "c1"],
+            chat_nm=["c0hat", "c1hat"],
+        )
+        self.calc_loglike_of_potential = True
+        self.rvs_potential_loglike = ["pot_chat", "pot_mhat"]
+
+    def _build(self):
+        """Builds and returns the model. Also sets self.model"""
+        self.coords.update(dict(oid=self.obs_m0.index.values))  # (i, )
+        self.n = len(self.obs_m0)
+        m_r = pd.concat(
+            [self.obs_m0[self.ft_en_m0], self.obs_m1[self.ft_en_m1]], axis=1
+        )
+        y_r = m_r.product(axis=1)
+        x0_r = self.obs_m0.drop(self.ft_en_m0, axis=1)
+        x1_r = self.obs_m1.drop(self.ft_en_m1, axis=1)
+
+        with pm.Model(coords=self.coords) as self.model:
+            # 0. Create (Mutable)Data containers for obs (Y, X)
+            m = pm.Data("m", m_r, dims=("oid", "m_nm"))
+            _ = pm.Data("y", y_r, dims="oid")  # convenience for post-processing
+            x0 = pm.Data("x0", x0_r, dims=("oid", "x0_nm"))
+            x1 = pm.Data("x1", x1_r, dims=("oid", "x1_nm"))
+
+            # 1. Define latent params for observed Marginals, regressing on F(X)
+            b_m0 = pm.Normal("beta_m0", mu=0.0, sigma=1.0, dims="x0_nm")
+            b_m1 = pm.Normal("beta_m1", mu=0.0, sigma=1.0, dims="x1_nm")
+            mu = pt.stack([pt.dot(x0, b_m0.T), pt.dot(x1, b_m1.T)], axis=1)
+            s = pm.InverseGamma("sigma", alpha=5.0, beta=4.0, dims="s_nm")
+
+            # 2. Define marginals and Evidence against observed
+            # Here we need to establish a LogNormal dist that we subsequently
+            # use to transform the observations. So we have to "evidence" the
+            # created dist object using a Potential (and minimise logp).
+            # Requires calc_loglike_of_potential = True
+            m_d = pm.LogNormal.dist(mu=mu, sigma=s, shape=(self.n, 2))
+            _ = pm.Potential("pot_mhat", pm.logp(m_d, m), dims=("oid", "mhat_nm"))
+
+            # 3. Transformation path pt1: Observed -> Uniform via Marginal CDF
+            u = pm.Deterministic("u", pt.exp(pm.logcdf(m_d, m)), dims=("oid", "u_nm"))
+
+            # 4. Transformation path pt2: Uniform -> Normal via Normal InvCDF
+            c = pm.Deterministic("c", mt.normal_icdf(u), dims=("oid", "c_nm"))
+            # y_cop_u_rv = pm.Normal.dist(mu=0., sigma=1.)
+            # c = pm.Deterministic("c", pm.icdf(y_cop_u_rv, u), dims=("oid", "c_nm"))
+
+            # 5. Create Latent Copula dist using a 2D MvNormal
+            # with estimated cov = chol.dot(chol.T)
+            sd = pm.InverseGamma.dist(alpha=5.0, beta=4.0)
+            chol, corr_, sdev_ = pm.LKJCholeskyCov("lkjcc", n=2, eta=2, sd_dist=sd)
+            c_d = pm.MvNormal.dist(mu=pt.zeros(2), chol=chol, shape=(self.n, 2))
+
+            # 6. Evidence transformed C against Latent Copula using Potential
+            #  because pymc TypeError: Variables that depend on other nodes
+            #  cannot be used for observed data (c)
+            # NOTE: MVNormal drops the last dim, becoming 1D !
+            # see explanations in https://github.com/pymc-devs/pymc/issues/7602
+            _ = pm.Potential("pot_chat", pm.logp(c_d, c, warn_rvs=True), dims="oid")
+
+            # 7. Post-process for forward / PPC estimation: create marginals mhat
+            # and joint product yhat.
+            # This allows `sample_prior_predictive`, `sample_posterior_predictive`,
+            # and overcomes issue: "UserWarning: The effect of Potentials on
+            # other parameters is ignored during prior predictive sampling."
+            # We follow the forward data-generating process as
+            # `synthetic.create_copula.CopulaBuilder`:
+            #  1. Transform latent MvN copula `c_d` (w/ est. covariance `chol`)
+            #    through a standard Normal CDF to get to Uniform [0, 1]
+            #  2. Transform Uniformed marginals to Observed via their iCDFs with
+            #    their estimated parameters
+            #  NOTE:
+            #  1. Using pm.logcdf(u_d, c_d) for convenience, then needs pt.exp()
+            #  2. Using mt.lognormal_icdf() to avoid CLIP issues
+            normal_d = pm.Normal.dist(mu=0.0, sigma=1.0, shape=(self.n, 2))
+            u_d = pt.exp(pm.logcdf(normal_d, c_d))
+            mhat = pm.Deterministic(
+                "mhat",
+                mt.lognormal_icdf(x=u_d, mu=mu, sigma=s),
+                dims=("oid", "mhat_nm"),
+            )
+            _ = pm.Deterministic("yhat", pm.math.prod(mhat, axis=1), dims="oid")
+
+        self.rvs_marg = ["sigma", "beta_m0", "beta_m1"]
+        self.rvs_lkjcc = ["lkjcc"]
+        self.rvs_pot = ["pot_chat", "pot_mhat"]
+        self.rvs_det = ["u", "c", "yhat", "lkjcc_stds", "lkjcc_corr"]
+        self.rvs_unobs = self.rvs_marg + self.rvs_lkjcc
+        self.rvs_ppc = ["mhat"]
+
+        return self.model
+
+
+class ModelA2(mt.BasePYMCModel):
+    """Full architecture (extends ModelA1)
+    New:
+      + Jacobian Adjustment on transformed observations (see step #7)
+    Core components used from oreum_core.model.BasePYMCModel
+    As used by 102_ModelA2.ipynb.
+    """
+
+    name = "mdla2"
+    version = "1.2.0"
+
+    def __init__(
+        self,
+        obs_m0: pd.DataFrame,
+        obs_m1: pd.DataFrame,
+        dfx_creatord: dict,
+        *args,
+        **kwargs,
+    ):
+        """Expects 2 dataframes for obs, each arranged as:
+            y ~ x, aka pd.concat((dfx_en, dfx_exs), axis=1)
+        Also expects `dfx_creatord` dict as {m0: dfx_creator, ...}
+        """
+        super().__init__(*args, **kwargs)
+        self.obs_nm = kwargs.pop("obs_nm", "obs")
+        self.rng = np.random.default_rng(seed=self.rsd)
+        self.sample_kws.update(dict(target_accept=0.80))
+
+        # set obs, coords, and do data validity checks
+        self.obs_m0 = obs_m0.copy()
+        self.obs_m1 = obs_m1.copy()
+        assert len(self.obs_m0) == len(self.obs_m1)
+        self.ft_en_m0 = dfx_creatord["m0"].ft_en
+        self.ft_en_m1 = dfx_creatord["m1"].ft_en
+        self.factor_map_m0 = dfx_creatord["m0"].factor_map
+        self.factor_map_m1 = dfx_creatord["m0"].factor_map
+        # setup coords with dict expansion and additional structural names
+        self.coords = dict(
+            x0_nm=self.obs_m0.columns.drop(self.ft_en_m0).values,
+            x1_nm=self.obs_m1.columns.drop(self.ft_en_m1).values,
+            s_nm=["s0", "s1"],
+            m_nm=["m0", "m1"],
+            mhat_nm=["m0hat", "m1hat"],
+            u_nm=["u0", "u1"],
+            c_nm=["c0", "c1"],
+            chat_nm=["c0hat", "c1hat"],
+        )
+        self.calc_loglike_of_potential = True
+        self.rvs_potential_loglike = ["pot_chat", "pot_mhat"]
+
+    def _build(self):
+        """Builds and returns the model. Also sets self.model"""
+        self.coords.update(dict(oid=self.obs_m0.index.values))  # (i, )
+        self.n = len(self.obs_m0)
+        m_r = pd.concat(
+            [self.obs_m0[self.ft_en_m0], self.obs_m1[self.ft_en_m1]], axis=1
+        )
+        y_r = m_r.product(axis=1)
+        x0_r = self.obs_m0.drop(self.ft_en_m0, axis=1)
+        x1_r = self.obs_m1.drop(self.ft_en_m1, axis=1)
+
+        with pm.Model(coords=self.coords) as self.model:
+            # 0. Create (Mutable)Data containers for obs (Y, X)
+            m = pm.Data("m", m_r, dims=("oid", "m_nm"))
+            _ = pm.Data("y", y_r, dims="oid")  # convenience for post-processing
+            x0 = pm.Data("x0", x0_r, dims=("oid", "x0_nm"))
+            x1 = pm.Data("x1", x1_r, dims=("oid", "x1_nm"))
+
+            # 1. Define latent params for observed Marginals, regressing on F(X)
+            b_m0 = pm.Normal("beta_m0", mu=0.0, sigma=1.0, dims="x0_nm")
+            b_m1 = pm.Normal("beta_m1", mu=0.0, sigma=1.0, dims="x1_nm")
+            mu = pt.stack([pt.dot(x0, b_m0.T), pt.dot(x1, b_m1.T)], axis=1)
+            s = pm.InverseGamma("sigma", alpha=5.0, beta=4.0, dims="s_nm")
+
+            # 2. Define marginals and Evidence against observed
+            # Here we need to establish a LogNormal dist that we subsequently
+            # use to transform the observations. So we have to "evidence" the
+            # created dist object using a Potential (and minimise logp).
+            # Requires calc_loglike_of_potential = True
+            m_d = pm.LogNormal.dist(mu=mu, sigma=s, shape=(self.n, 2))
+            _ = pm.Potential("pot_mhat", pm.logp(m_d, m), dims=("oid", "mhat_nm"))
+
+            # 3. Transformation path pt1: Observed -> Uniform via Marginal CDF
+            u = pm.Deterministic("u", pt.exp(pm.logcdf(m_d, m)), dims=("oid", "u_nm"))
+
+            # 4. Transformation path pt2: Uniform -> Normal via Normal InvCDF
+            c = pm.Deterministic("c", mt.normal_icdf(u), dims=("oid", "c_nm"))
+            # y_cop_u_rv = pm.Normal.dist(mu=0., sigma=1.)
+            # c = pm.Deterministic("c", pm.icdf(y_cop_u_rv, u), dims=("oid", "c_nm"))
+
+            # 5. Create Latent Copula dist using a 2D MvNormal
+            # with estimated cov = chol.dot(chol.T)
+            sd = pm.InverseGamma.dist(alpha=5.0, beta=4.0)
+            chol, corr_, sdev_ = pm.LKJCholeskyCov("lkjcc", n=2, eta=2, sd_dist=sd)
+            c_d = pm.MvNormal.dist(mu=pt.zeros(2), chol=chol, shape=(self.n, 2))
+
+            # 6. Evidence transformed C against Latent Copula using Potential
+            #  because pymc TypeError: Variables that depend on other nodes
+            #  cannot be used for observed data (c)
+            # NOTE: MVNormal drops the last dim, becoming 1D !
+            # see explanations in https://github.com/pymc-devs/pymc/issues/7602
+            _ = pm.Potential("pot_chat", pm.logp(c_d, c, warn_rvs=True), dims="oid")
+
+            # 7. Jacobian adjustment to reduce variance in C due to change in
+            #  volume due to transformation `M -> U -> C`
+            _ = pm.Potential(
+                "pot_jcd_c", mt.get_log_jcd_scan(c, m, upstream_rvs=[mu, s]), dims="oid"
+            )
+
+            # 8. Post-process for forward / PPC estimation: create marginals mhat
+            # and joint product yhat.
+            # This allows `sample_prior_predictive`, `sample_posterior_predictive`,
+            # and overcomes issue: "UserWarning: The effect of Potentials on
+            # other parameters is ignored during prior predictive sampling."
+            # We follow the forward data-generating process as
+            # `synthetic.create_copula.CopulaBuilder`:
+            #  1. Transform latent MvN copula `c_d` (w/ est. covariance `chol`)
+            #    through a standard Normal CDF to get to Uniform [0, 1]
+            #  2. Transform Uniformed marginals to Observed via their iCDFs with
+            #    their estimated parameters
+            #  NOTE:
+            #  1. Using pm.logcdf(u_d, c_d) for convenience, then needs pt.exp()
+            #  2. Using mt.lognormal_icdf() to avoid CLIP issues
+            normal_d = pm.Normal.dist(mu=0.0, sigma=1.0, shape=(self.n, 2))
+            u_d = pt.exp(pm.logcdf(normal_d, c_d))
+            mhat = pm.Deterministic(
+                "mhat",
+                mt.lognormal_icdf(x=u_d, mu=mu, sigma=s),
+                dims=("oid", "mhat_nm"),
+            )
+            _ = pm.Deterministic("yhat", pm.math.prod(mhat, axis=1), dims="oid")
+
+        self.rvs_marg = ["sigma", "beta_m0", "beta_m1"]
+        self.rvs_lkjcc = ["lkjcc"]
+        self.rvs_pot = ["pot_chat", "pot_mhat", "pot_jcd_c"]
+        self.rvs_det = ["u", "c", "yhat", "lkjcc_stds", "lkjcc_corr"]
+        self.rvs_unobs = self.rvs_marg + self.rvs_lkjcc
+        self.rvs_ppc = ["mhat"]
+
+        return self.model
